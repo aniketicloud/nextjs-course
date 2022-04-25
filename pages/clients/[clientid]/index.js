3893f09a7e59aa54e968a4a6833eeeb36ff63fa9
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-04-26 00:20:43 +0530

    Navigating Programmatically

--- pages/clients/[clientid]/index.js
+++ pages/clients/[clientid]/index.js
@@ -2,12 +2,28 @@ import { useRouter } from "next/router";
 
 const ClientProjectsPage = () => {
   const router = useRouter();
+
+  const loadProjectHandler = () => {
+    // load data . . .
+    // and then navigate away
+    const clientid = router.query.clientid;
+    // router.push(`/clients/${clientid}/projecta`);
+    router.push({
+      pathname: "/clients/[clientid]/[clientprojectid]",
+      query: {
+        clientid: clientid,
+        clientprojectid: "projecta",
+      },
+    });
+  };
+
   console.log(router.query);
   return (
     <div>
       <h1>All the projects of a given or selected Client</h1>
       <h3>e.g. List of all projects under selected client</h3>
       <br />
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -9,7 +9,6 @@ const ClientsPage = () => {
     <div>
       <h1>Clients Page</h1>
       <h3>e.g. List of all clients</h3>
-      {/* href={`/clients/${client.id}`} */}
 
       <ul>
         {clients.map((client) => {
