6bd3a2526173fd120fa8a43696ec8f58567f8a87
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-04-25 21:07:13 +0530

    Navigating To Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,10 +1,24 @@
+import Link from "next/link";
+
 const ClientsPage = () => {
+  const clients = [
+    { id: "nike", name: "Nike" },
+    { id: "olympus", name: "Olympus" },
+  ];
   return (
     <div>
-      <h1>
-        Clients Page
-      </h1>
+      <h1>Clients Page</h1>
       <h3>e.g. List of all clients</h3>
+
+      <ul>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
