7d0d2211dcc580b8d7289ebc5e3ca72ea65e2374
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-04-25 20:52:13 +0530

    Navigating with the "Link" Component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 const HomePage = () => {
   return (
     <div>
       <h1>Hello Next World . . . !</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">clients</Link>
+        </li>
+      </ul>
     </div>
   );
 };
